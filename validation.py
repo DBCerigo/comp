8a9102ae8d3b7677c0942486b098fa71c4f9a944
dbcerigo <dbcerigo@gmail.com>
2019-01-31 18:23:24 +0000

    Implement reading + passing train data

--- validation.py
+++ validation.py
@@ -4,11 +4,14 @@ Module for validation computation and validation results storing+reading
 """
 import numpy as np
 from sklearn import model_selection
-
+import pandas as pd
+import pyarrow.parquet as pq
 
 # set random seed for reproduce
 np.random.seed(41732)
 # WARN: if using multithreading see https://stackoverflow.com/questions/31057197/should-i-use-random-seed-or-numpy-random-seed-to-control-random-number-gener
+
+TEST_CUTOFF = 100
 
 
 def run(model, hyperparams=None):
@@ -25,13 +28,77 @@ def run(model, hyperparams=None):
 
     Returns
     --------
-    None
+    Validation results object.
 
     Writes
     -------
     Validation results to `/results`
     """
-    scores = cross_val_score(model, None, None, cv=8)
+    scores = cross_val_score(
+            model,
+            read_data(),
+            read_targets(),
+            groups=read_metadata().id_measurement,
+            cv=8)
+    return scores
+
+def read_targets():
+    """
+    Read target data.
+
+    Returns
+    --------
+    list
+    """
+    metadata = read_metadata()
+    return metadata.target.values
+
+def read_metadata():
+    """
+    Read metadata file.
+
+    Returns
+    --------
+    dataframe
+        Pandas df of metadata.
+    """
+    train_meta = pd.read_csv(_as_absolute('../raw_data/metadata_train.csv'))
+    train_meta = train_meta[:TEST_CUTOFF]
+    return train_meta
+
+def read_data():
+    """
+    Read data file.
+
+    Returns
+    --------
+    dataframe
+        Pandas df of data.
+    """
+    train = pq.read_table(
+            '../raw_data/train.parquet',
+            columns=[str(i) for i in range(TEST_CUTOFF)]
+            ).to_pandas()
+    train = train.T
+    return train
+
+
+def _as_absolute(path):
+    """
+    Convert path to absolute from file called in.
+    Parameters
+    ----------
+    path: str
+        Path to make absolute.
+
+    Returns
+    --------
+    str
+        Absolute path.
+    """
+    return os.path.abspath(os.path.join(__file__, "../", path))
+
+
 
 def _store_validation_result():
     pass
