91656dc0fe4086dd8153af8990ae8a5b8f8275cd
dbcerigo <dbcerigo@gmail.com>
2019-01-31 18:06:20 +0000

    Add setting random seed

--- validation.py
+++ validation.py
@@ -6,6 +6,11 @@ import numpy as np
 from sklearn import model_selection
 
 
+# set random seed for reproduce
+np.random.seed(41732)
+# WARN: if using multithreading see https://stackoverflow.com/questions/31057197/should-i-use-random-seed-or-numpy-random-seed-to-control-random-number-gener
+
+
 def run(model, hyperparams=None):
     """
     Main validation function.
